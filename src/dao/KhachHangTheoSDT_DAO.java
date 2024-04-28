370247e480310d719ddf7b6fdae2655a8cbc29f1
unknown <sonjiny2004@gmail.com>
2024-04-28 22:58:08 +0700

    ConnectDB + SanPham_DAO

--- src/dao/KhachHangTheoSDT_DAO.java
+++ src/dao/KhachHangTheoSDT_DAO.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-import connect.ConnectDB;
+import connectDB.ConnectDB;
 import entity.KhachHang;
 
 //import connect.ConnectDB;
